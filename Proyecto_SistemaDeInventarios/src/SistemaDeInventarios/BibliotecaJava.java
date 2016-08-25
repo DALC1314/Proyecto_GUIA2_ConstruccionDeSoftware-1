23a7f8b6a9ea73e83017fe854d73fc670bb159f5
Luis Carlos Gomez <lgomezca1327@universidadean.edu.co>
2016-08-25 14:44:54 -0500

    primer commit del proyecto

---
+++ Proyecto_SistemaDeInventarios/src/SistemaDeInventarios/BibliotecaJava.java
@@ -0,0 +1,13 @@
+
+package SistemaDeInventarios;
+
+import java.awt.Color;
+import javax.swing.JOptionPane;
+
+public class BibliotecaJava {
+    public static void main(String[] args) {      
+        System.out.println("Proyecto construccion de software");
+        MenuPrincipal v1 = new MenuPrincipal();
+        //v1.setBackground(new Color(255,0,255)); //para cambiar color
+    }
+}
